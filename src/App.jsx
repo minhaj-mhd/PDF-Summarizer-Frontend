8577e9c6358f2efd8926727ea05a8f51e4bec430
minhaj-mhd <minhajmuhamad@gmail.com>
2025-04-20 10:55:26 +0530

    added structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-center py-4'>
+      <h1 className='text-3xl font-bold underline'>PDF Summariser</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
